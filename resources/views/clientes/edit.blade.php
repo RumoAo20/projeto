9c8f02f86add9ba702cd43689a66e72242d43565
filipemn1 <41974153+filipemn1@users.noreply.github.com>
2018-08-07 18:04:40 +0100

    Add files via upload

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -2,32 +2,35 @@
 @section('contentheader_title')
     Perfil do Cliente
 @endsection
-24
 @section('main-content')
     <section class="content">
         <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-5">
                 <!-- Profile Image -->
                 <div class="box box-primary">
                     <div class="box-body box-profile">
                         <img class="profile-user-img img-responsive img-circle"
                              src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
-                        <h3 class="profile-username text-center">{{ $cliente->nome }}</h3>
 
-                        <ul class="list-group list-group-unbordered">
-                            <li class="list-group-item">
-                                <b>Nif</b> <a class="pull-right">{{ $cliente->nif }}</a>
-                            </li>
-                            <li class="list-group-item">
-                                <b>Morada</b> <a class="pull-right">{{ $cliente->morada }}</a>
-                            </li>
-                            <li class="list-group-item">
-                                <b>Nº tlm</b> <a class="pull-right">{{ $cliente->telemovel }}</a>
-                            </li>
-                            <li class="list-group-item">
-                                <b>Email</b> <a class="pull-right">{{ $cliente->email }}</a>
-                            </li>
-                        </ul>
+                        {{ Form::model($cliente, array('route' => array('clientes.update', $cliente->id), 'method' => 'PUT')) }}
+
+                        <li class="list-group-item">
+                            <a class="profile-username text-center"></a><b>Nome {{ Form::text('nome', null, array('class' => 'form')) }} {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul></b>
+                        </li>
+                        <li class="list-group-item">
+                            <a class="pull-right"></a><b>Nif {{ Form::text('nif', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
+                        </li>
+                        <li class="list-group-item">
+                            <a class="pull-right"></a><b>Morada {{ Form::text('morada', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
+                        </li>
+                        <li class="list-group-item">
+                            <a class="pull-right"></a><b>Nº tlm {{ Form::text('telemovel', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
+                        </li>
+                        <li class="list-group-item">
+                            <a class="pull-right"></a><b>Email{{ Form::text('email', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
+                        </li>
+                            </ul>
+                        {{ Form::close() }}
                     </div>
                     <!-- /.box-body -->
                 </div>
