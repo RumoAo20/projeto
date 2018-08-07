a0135b4d457a685f87587b25484f14062c70bd3c
filipemn1 <41974153+filipemn1@users.noreply.github.com>
2018-08-07 18:07:30 +0100

    Add files via upload

--- app/Colaborador.php
+++ app/Colaborador.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Colaborador extends Model
 {
     protected $table = 'users';
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
 }
