f363778df88cc8104ad0c52b08bc023910b2847e
RumoAo20 <41999636+RumoAo20@users.noreply.github.com>
2018-08-08 14:44:13 +0100

    Add files via upload

--- app/Produto.php
+++ app/Produto.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     protected $table = 'produto';
+
+    protected $fillable = [
+        'numero_registo', 'chnm', 'descricao_chnm', 'nome_forma_dosagem',
+    ];
 }
