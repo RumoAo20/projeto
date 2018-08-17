1bd7c0e173dd52bd9373481daf3ed136bac9539d
RumoAo20 <41999636+RumoAo20@users.noreply.github.com>
2018-08-17 16:08:16 +0100

    Add files via upload

--- resources/views/fornecedores/edit.blade.php
+++ resources/views/fornecedores/edit.blade.php
@@ -3,6 +3,28 @@
     Perfil do Fornecedor
 @endsection
 @section('main-content')
+
+
+    {{--Lista com erros do formulario--}}
+
+    <div class ='alert alert-error'>
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+
+
+    {{--Mensagem de sucesso--}}
+    @if(Session::has('message'))
+        <div class="alert alert-{{ Session::get('message-type') }} alert-dismissable">
+            <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+            <i class="glyphicon glyphicon-{{ Session::get('message-type') == 'success' ? 'ok' : 'remove'}}"></i> {{ Session::get('message') }}
+        </div>
+    @endif
+
+
     <section class="content">
         <div class="row">
             <div class="col-md-5">
@@ -15,27 +37,27 @@
                         {{ Form::model($fornecedor, array('route' => array('fornecedores.update', $fornecedor->id), 'method' => 'PUT')) }}
 
                         <li class="list-group-item">
-                        <a class="profile-username text-center"></a><b>Nome {{ Form::text('nome', null, array('class' => 'form')) }} {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul></b>
+                            <a class="profile-username text-center"></a><b>Nome {{ Form::text('nome', null, array('class' => 'form')) }} {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul></b>
                         </li>
                         <li class="list-group-item">
-                        <a class="pull-right"></a><b>Nif {{ Form::text('nif', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
+                            <a class="pull-right"></a><b>Nif {{ Form::text('nif', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
+                        </li>
+                        <li class="list-group-item">
+                            <a class="pull-right"></a><b>Morada {{ Form::text('morada', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
+                        </li>
+                        <li class="list-group-item">
+                            <a class="pull-right"></a><b>Nº tlm {{ Form::text('telemovel', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
+                        </li>
+                        <li class="list-group-item">
+                            <a class="pull-right"></a><b>Email{{ Form::text('email', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
+                        </li>
+                        <li class="list-group-item">
+                            <a class="pull-right"></a><b>Tipo de produto{{ Form::text('tipoProduto', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
+                        </li>
+                        <li class="list-group-item">
+                            <a class="pull-right"></a><b>Designação comercial {{ Form::text('designacaoComercial', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered">
                                 </li>
-                            <li class="list-group-item">
-                                <a class="pull-right"></a><b>Morada {{ Form::text('morada', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
-                            </li>
-                            <li class="list-group-item">
-                                <a class="pull-right"></a><b>Nº tlm {{ Form::text('telemovel', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
-                            </li>
-                            <li class="list-group-item">
-                                <a class="pull-right"></a><b>Email{{ Form::text('email', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
-                            </li>
-                            <li class="list-group-item">
-                                <a class="pull-right"></a><b>Tipo de produto{{ Form::text('tipoProduto', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered"></ul>
-                            </li>
-                            <li class="list-group-item">
-                                <a class="pull-right"></a><b>Designação comercial {{ Form::text('designacaoComercial', null, array('class' => 'form')) }}</b> {{Form::button('<i class="fa fa-pencil fa fa-white"></i>', ['class'=>'btn btn-info', 'type'=>'submit']) }}<ul class="list-group list-group-unbordered">
-                            </li>
-                        </ul>
+                            </ul>
                         {{ Form::close() }}
                     </div>
                     <!-- /.box-body -->
